7b566a59d19c82a613a3d8e91920fe51ae01ffb7
Alexander Wang (Class of 2025) <250080aw@salesianum.org>
2023-08-29 13:43:32 -0400

    Test 2 example repository

---
+++ TestJDK.java
@@ -0,0 +1,17 @@
+package GitPractice;
+
+        TestJDK
+
+
+        public class TestJDK{
+
+        public static void main(String[] args) {
+    String name = "john";
+        System.out.print(name.toUpperCase());
+        }
+        public int test(){
+                return 7;
+                                                                    
+    }
+    }
+                                                            
